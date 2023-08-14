b47386667db4534cfab3082f33da8fee236268da
Ana Belen <abilabae@gmail.com>
2023-08-13 21:49:28 -0400

    proyecto de prueba back-front

--- client/src/components/Formulario.jsx
+++ client/src/components/Formulario.jsx
@@ -1,54 +1,72 @@
 import React from 'react'
 import { useState } from 'react'
+import {useNavigate} from "react-router-dom";
+import axios from 'axios'
+
 
 const Formulario = () => {
-const [nombre, setNombre]=useState("")
-const [email, setEmail]=useState("")
-const [direccion, setDireccion]=useState("")
-const [password, setPassword]=useState("")
 
-const handleSubmit=(e)=>{
-    e.preventDefault()
+const navigate=useNavigate();
+const[usuario, setUsuario]= useState({});
 
-    
-    try {
-        const body= {nombre, email, direccion, password}
-        fetch ("http://localhost:3002/usuarios", {
-            method:"POST",
-            headers:{"Content-type": "application/json"},
-            body: JSON.stringify(body)
-        })
-    } catch (error) {
-        console.log(error)
-    }
+const handleSetUsuario=({ target:{value, name}})=>{
+  const field={};
+  field[name]=value;
+  setUsuario({...usuario, ...field});
+};
+
+const registrarUsuario=async()=>{
+  const urlServer="http://localhost:3000";
+  const endpoint="/usuarios"
+  try {
+    await axios.post(urlServer + endpoint, usuario);
+    alert("Usuario registrado con exito");
+    navigate("/login");
+  } catch (error) {
+    alert("Algo salió mal...")
+    console.log(error)
+  }
 }
+
+      
+//     try {
+//         const body= {nombre, email, direccion, password}
+//         fetch ("http://localhost:3002/usuarios", {
+//             method:"POST",
+//             headers:{"Content-type": "application/json"},
+//             body: JSON.stringify(body)
+//         })
+//     } catch (error) {
+//         console.log(error)
+//     }
+
 
 
   return (
     <div>
-      <form onSubmit={handleSubmit}>
+      <form >
 
         <div>
             <label className='form-label' htmlFor='title'>Nombre</label>
-            <input className="form-control" type="text" id="title" value={nombre} onChange={(e)=>setNombre(e.target.value)}/>
+            <input className="form-control" type="text" id="title" value={usuario.nombre} onChange={handleSetUsuario}/>
         </div>
 
         <div>
             <label className='form-label' htmlFor='title'>email</label>
-            <input className="form-control" type="email" id="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+            <input className="form-control" type="email" id="email" value={usuario.email} onChange={handleSetUsuario}/>
         </div>
 
         <div>
             <label className='form-label' htmlFor='title'>Direccion</label>
-            <input className="form-control" type="addres" id="addres" value={direccion} onChange={(e)=>setDireccion(e.target.value)}/>
+            <input className="form-control" type="addres" id="addres" value={usuario.direccion} onChange={handleSetUsuario}/>
         </div>
 
         <div>
             <label className='form-label' htmlFor='title'>Password</label>
-            <input className="form-control" type="password" id="password" value={password} onChange={(e)=>setPassword(e.target.value)}/>
+            <input className="form-control" type="password" id="password" value={usuario.password} onChange={handleSetUsuario}/>
         </div>
 
-        <button className='btn btn-primary'>Registrarse</button>
+        <button className='btn btn-primary' onClick={registrarUsuario}>Registrarse</button>
 
       </form>
     </div>
--- server/app.js
+++ server/app.js
@@ -3,7 +3,7 @@ const cors=require('cors')
 const router=require('./routes')
 
 const app=express()
-const PORT=process.env.PORT || 3002
+const PORT=process.env.PORT || 3000
 
 app.use(express.json())
 app.use(cors())
